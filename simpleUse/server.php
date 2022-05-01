17a81219d3cf0e26e243fcb18f9f595a64961f75
lvzhenchao <1370164516@qq.com>
2022-05-01 15:55:37 +0800

    Update server.php

--- simpleUse/server.php
+++ simpleUse/server.php
@@ -18,7 +18,21 @@ Class Server {
             $str = "hello rpc ".date("Y-m-d H:i:s")."\n";
 
             $data = socket_read($conSocket, 1024);
-            socket_write($conSocket , $data, strlen($data));
+//            socket_write($conSocket , $data, strlen($data));
+
+            preg_match("/RPC-CLASS:(.*)/", $data, $class);
+            preg_match("/RPC-METHOD:(.*)/", $data, $method);
+            preg_match("/RPC-PARAM:(.*)/", $data, $param);
+
+            $class  = $class[1];
+            $method = $method[1];
+            $param  = $param[1];
+            require_once __DIR__."/class/".$class.".php";
+
+            $obj = new $class;
+            $res = $obj->$method();
+
+            socket_write($conSocket , $res, strlen($res));
         }
 
     }
