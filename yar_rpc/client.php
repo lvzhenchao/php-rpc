4d74245727eb79fbf4c71f933eafc33f18bd0d30
lvzhenchao <1370164516@qq.com>
2022-05-02 11:19:49 +0800

    Update client.php

--- yar_rpc/client.php
+++ yar_rpc/client.php
@@ -4,4 +4,4 @@ $client = new Yar_Client('http://yar.com/server.php');
 $client->SetOpt(YAR_OPT_PACKAGER, "json");
 $client->SetOpt(YAR_OPT_CONNECT_TIMEOUT, 1000);
 //var_dump($client);
-$client->login([]);
+echo $client->login();
